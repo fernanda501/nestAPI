388db0d10146343ff4878e11deb103e55a8c6a55
iTzSebasVFX <juansebastian320519@gmail.com>
2025-06-20 15:44:05 -0500

    configuracion variables entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TaskModule } from './task/task.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports:[ConfigModule],
+      inject:[ConfigService],
+      useFactory:(Config:ConfigService)=>({
+        type:'mysql',
+        host: Config.get('DB_HOST'),
+        port: Config.get('DB_PORT'),
+        username: Config.get('DB_USER'),
+        database: Config.get('DB_NAME'),
+        entities:[__dirname + '/**/**/*.entity{.ts,.js']
+      })
+    }),
+    TaskModule,
+  ],
 })
 export class AppModule {}
